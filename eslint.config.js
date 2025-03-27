c9ea725644ad0859da2b441d2c6550006836d172
Mat3uszK <118600276+Mat3uszK@users.noreply.github.com>
2025-03-27 12:08:46 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,3 @@
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
 {
   "settings": {
     "react": {
@@ -44,4 +41,3 @@ export default defineConfig([
     ]
   }
 }
-]);
